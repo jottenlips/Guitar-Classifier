db55ccf8dd70c8fa4b51f2bd5c9c0af8a3a50057
johno <johno@aloompa.com>
2018-06-22 13:05:07 -0500

    new model optimized

--- scripts/converter.py
+++ scripts/converter.py
@@ -5,7 +5,7 @@
 
 
 import tfcoreml as tf_converter
-tf_converter.convert(tf_model_path = '/Users/john/ml-exercise/tf_files/retrained_graph.pb',
+tf_converter.convert(tf_model_path = '/Users/john/ml-exercise/tf_files/graph_optimized.pb',
 	mlmodel_path = 'MyModel.mlmodel',
 	output_feature_names = ['final_result:0'],
 	input_name_shape_dict = {'input:0':[1,224,224,3]},
