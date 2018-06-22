340270b62a80ba6f3b8ed50fc3513f6abb6b8a0c
johno <johno@aloompa.com>
2018-06-22 12:50:30 -0500

    new model

--- scripts/converter.py
+++ scripts/converter.py
@@ -9,7 +9,7 @@ tf_converter.convert(tf_model_path = '/Users/john/ml-exercise/tf_files/retrained
 	mlmodel_path = 'MyModel.mlmodel',
 	output_feature_names = ['final_result:0'],
 	input_name_shape_dict = {'input:0':[1,224,224,3]},
-	image_input_names = ['input:0'],
+	image_input_names = ['Mul:0'],
 	class_labels = '/Users/john/ml-exercise/tf_files/retrained_labels.txt'
 )
 
