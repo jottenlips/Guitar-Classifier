9585966dcf3cc6153ffe39f6a28b54880a22d7f7
johno <johno@aloompa.com>
2018-06-22 09:02:12 -0500

    current model

--- scripts/converter.py
+++ scripts/converter.py
@@ -5,12 +5,12 @@
 
 
 import tfcoreml as tf_converter
-tf_converter.convert(tf_model_path = '/Users/johnottenlips/ml-exercise/tf_files/retrained_graph.pb',
+tf_converter.convert(tf_model_path = '/Users/john/ml-exercise/tf_files/retrained_graph.pb',
 	mlmodel_path = 'MyModel.mlmodel',
 	output_feature_names = ['final_result:0'],
 	input_name_shape_dict = {'input:0':[1,224,224,3]},
 	image_input_names = ['input:0'],
-	class_labels = '/Users/johnottenlips/ml-exercise/tf_files/retrained_labels.txt'
+	class_labels = '/Users/john/ml-exercise/tf_files/retrained_labels.txt'
 )
 
 
