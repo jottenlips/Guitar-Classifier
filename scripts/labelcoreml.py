8f0637f41145e3ed85fcdfd2e8acf400298289df
John Ottenlips <johno@aloompa.com>
2020-06-28 21:13:12 -0500

    ml model

--- scripts/labelcoreml.py
+++ scripts/labelcoreml.py
@@ -2,12 +2,12 @@
 from PIL import Image
 import tfcoreml as tf_converter
 
-coreml_model = tf_converter.convert(tf_model_path = '/Users/john/ml-exercise/tf_files/retrained_graph.pb',
+coreml_model = tf_converter.convert(tf_model_path = '/Users/johnottenlips/ml-exercise/tf_files/retrained_graph.pb',
 	mlmodel_path = 'MyModel.mlmodel',
 	output_feature_names = ['final_result:0'],
 	input_name_shape_dict = {'input:0':[1,224,224,4]},
 	image_input_names = ['input:0'],
-	class_labels = '/Users/john/ml-exercise/tf_files/retrained_labels.txt',
+	class_labels = '/Users/johnottenlips/ml-exercise/tf_files/retrained_labels.txt',
 	image_scale=2/255.0,
 red_bias=-1,
 green_bias=-1,
@@ -23,22 +23,13 @@ blue_bias=-1
 # --output_names=final_result
 
 
-# Now we're ready to test out the CoreML model with a real image!
-# Load an image
+
 import PIL
-import requests
-from io import BytesIO
-from matplotlib.pyplot import imshow
+
 import numpy as np
-# This is an image of a golden retriever from Wikipedia
-# img_url = ''
-# response = requests.get(img_url)
-# img = PIL.Image.open(response.content)
-# imshow(np.asarray(img))
-im = Image.open("../../Desktop/mine.jpg")
-# Run CoreML prediction
-# Pay attention to '__0'. We change ':0' to '__0' to make sure 
-# MLModel's generated Swift/Obj-C code is semantically correct
+
+im = Image.open("../../Desktop/mustang.jpg")
+
 final_size = 224;
 size = im.size
 ratio = float(final_size) / max(size)
